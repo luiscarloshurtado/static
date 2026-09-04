bac7b22d81e5f355660a4ce2898e99e205d663d7
luiscarloshurtado <luiscarloshurtado@live.com>
2026-09-04 15:27:54 -0500

    Init Interfaz

---
+++ app/js/env.js
@@ -0,0 +1,4 @@
+Go.env({
+  app_name: "Luigi OS",
+  app_description: "The Cloud OS",
+});
---
+++ app/js/views.js
@@ -0,0 +1,14 @@
+Go.views({
+  "/app/games/*": {
+    name: "home",
+    html: "/app/html/home.html",
+    target: ".app.body",
+    title: `${Go.lang("app_name")} - ${Go.lang("app_description")}`,
+    routes: {
+      "/*": {
+        name: "home",
+        fn: () => Go.do("home:init"),
+      },
+    },
+  },
+});
