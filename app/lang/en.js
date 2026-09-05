0c0a46b294252b98aaf8d8425fcc4529eb914f94
luiscarloshurtado <luiscarloshurtado@live.com>
2026-09-04 21:52:38 -0500

    Auto Update

--- app/lang/en.js
+++ app/lang/en.js
@@ -2,4 +2,5 @@ export default {
   app_name: "Luigi OS",
   app_description: "The Cloud OS",
   "404_not_found": "404 Not found",
+  operative_system: "Operative System",
 };
--- app/lang/es.js
+++ app/lang/es.js
@@ -2,4 +2,5 @@ export default {
   app_name: "Luigi OS",
   app_description: "The Cloud OS",
   "404_not_found": "404 No encontrado",
+  operative_system: "Sistema Operativo",
 };
