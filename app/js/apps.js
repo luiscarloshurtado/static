21f2e022cc430b18fd63978d35a6e1b0e9303ae1
luiscarloshurtado <luiscarloshurtado@live.com>
2026-09-05 13:52:46 -0500

    Auto Update

--- app/js/apps.js
+++ app/js/apps.js
@@ -3,7 +3,7 @@ Go.set("apps:list", function () {
     {
       name: Go.lang("operative_system"),
       icon: "/app/img/icons/512x512.png",
-      src: "https://www.luigios.com/app/desktop/",
+      src: "https://www.luigios.com/app/luigios/",
       description: Go.lang("operative_system_description"),
     },
     {
@@ -11,6 +11,12 @@ Go.set("apps:list", function () {
       icon: "/app/img/all/games.png",
       src: "https://www.luigios.com/app/games/",
       description: Go.lang("games_description"),
+    },
+    {
+      name: Go.lang("apps"),
+      icon: "/app/img/all/apps.png",
+      src: "https://www.luigios.com/app/desktop/",
+      description: Go.lang("apps_description"),
     },
   ];
 });
