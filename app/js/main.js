040175b19dba0d85fa0af867abf7af189fe397b6
luiscarloshurtado <luiscarloshurtado@live.com>
2026-09-05 14:09:29 -0500

    Auto Update

--- app/js/main.js
+++ app/js/main.js
@@ -3,13 +3,13 @@ import "./views.js";
 import "./apps.js";
 import "./home.js";
 
-const lang = Go.if({
-  cond: () => ["en", "es"].includes(Go.lang().current()),
-  true: () => Go.lang().current(),
-  else: "en",
-});
-
 window.addEventListener("load", () => {
+  const lang = Go.if({
+    cond: () => ["en", "es"].includes(Go.lang().current()),
+    true: () => Go.lang().current(),
+    else: () => "en",
+  });
+
   Go.import(`/app/lang/${lang}.js`, (module) => {
     Go.lang(module);
     Go.do("nav:start");
